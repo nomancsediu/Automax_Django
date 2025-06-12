0a7af93287b397ae1e5aba1bd5c197973637c6e7
Abdullah Al Noman <abd.noman720@gmail.com>
2025-06-13 04:02:33 +0600

    abd

--- src/main/views.py
+++ src/main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def main_view(request):
-    return HttpResponse("<h1>Welcome to Automax!</h1>")
+    return render(request, "views/main.html", {"name":"AutoMax!"})
